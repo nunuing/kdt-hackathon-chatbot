9437b3c2084e80322c64b055dba4e463e903363d
eycho <chw3872@gmail.com>
2024-10-28 17:53:35 +0900

    (은유) add assets

---
+++ src/components/main.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <img id="chatbot_logo" src="../assets/icon/chatbot.png">
+        <div><span id="title_poing">'오손녀'</span>
+            가 서류 발급을 도와드릴게요! 
+        </div>
+    </div>
+</template>
